418317086af7bf9b471851755c2694e9c6dce0e5
Lornng <ngjianjia02@gmail.com>
2023-05-10 03:57:32 +1000

    World Map, one section left
    Zoom function, reset button, country name tooltip

--- Home.js
+++ Home.js
@@ -9,38 +9,48 @@ function init(){
 
     var path = d3.geoPath()
                     .projection(projection);
-    
 
-    var color = d3.scaleQuantize
-
-    
-    //APPEND
     var svg = d3.select("#world_map")
                 .append("svg")
                 .attr("width", w)
                 .attr("height", h);
 
-        //Include <g> element in SVG
+    //Include <g> element in SVG
     var g = svg.append("g");
     
+    var zoom = d3.zoom()
+                    .on("zoom", zoomed);
+
+    svg.call(zoom);
+
+    function zoomed(){
+        g.attr('transform', d3.event.transform);
+    }
+
+    //Add reset button
+    d3.select("#reset").on("click", function() {
+        svg.transition().duration(750).call(zoom.transform, d3.zoomIdentity);
+    });
+        
+
     //Load TSV and JSON data concurrenntly using Promise.all()
     Promise.all([
-        d3.tsv("https://unpkg.com/browse/world-atlas@1.1.4/world/110m.tsv"),
-        d3.json("https://unpkg.com/world-atlas@1.1.4/world/110m.json")
+        d3.tsv("https://unpkg.com/world-atlas@1.1.4/world/50m.tsv"),
+        d3.json("https://unpkg.com/world-atlas@1.1.4/world/50m.json")
     ]).then(function (results){
         var tsvData = results[0];
         var topoData = results[1];
 
-        console.log(tsvData);
-        console.log(topoData);
+        //convert TopoJSON to GeoJSON
+        var countries = topojson.feature(topoData, topoData.objects.countries);
 
-    })
-
-    //READ IN DATA/JSON
-    d3.json("https://unpkg.com/world-atlas@1.1.4/world/110m.json").then(function(data){
-        //topojson.feature(TopoJSON object, TopoJSON object that want to convert to GeoJSON)
-        var countries = topojson.feature(data, data.objects.countries);
-
+        //use this to bring in the country name
+        var countryName = tsvData.reduce(function(obj, d) {
+            obj[d.iso_n3] = d.name;
+            return obj;
+          }, {});
+        
+        //Append <path> element to the <g> element
         g.selectAll("path")
             .data(countries.features)
             .enter()
@@ -48,8 +58,23 @@ function init(){
             .attr("class", "country")
             //set each d attribute of the of the 'path' as country
             .attr("d", path)
-            .append("title")
-            //.text(console.log(d))
+            .on("mouseover", function(d) {
+                //Get the country name from the countryName object
+                var country = countryName[d.id];
+                //Update the text element with the country name
+                svg.append('text')
+                    .attr('id', 'tooltip')
+                    .attr('x', 10)
+                    .attr('y', 30)
+                    .attr('fill', 'white')
+                    .style('font-size', '20px')
+                    .text("Country Name: " + countryName[d.id]);
+            })
+            .on("mouseout", function() {
+                //Reset the text element to an empty string
+                svg.select('#tooltip').remove();
+            });
     })
+
 }
 window.onload = init; 
